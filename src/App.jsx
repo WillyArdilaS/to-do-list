cebdf0cddef0e9479356166b2cd45fac424085c2
WillyArdilaS <williamardilasanchez@gmail.com>
2022-09-11 16:57:37 -0500

    Tasks created in list

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import Form from "./Components/Form";
 import Header from "./Components/Header";
 import ToDoList from "./Components/ToDoList";
 
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [actualTask, setActualTask] = useState({});
+
   return (
     <div className="container mx-auto mt-16">
       <Header/>
+      
       <main className="mt-12 md:flex">
-        <Form/>
-        <ToDoList/>
+        <Form tasks={tasks} setTasks={setTasks} actualTask={actualTask} />
+        <ToDoList tasks={tasks} setActualTask={setActualTask}/>
       </main>
     </div>
   );
--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,24 +1,30 @@
-const Task = () => {
+const Task = ({task, setActualTask}) => { 
+  const {title, date, description} = task;
+
   return (
     <div className="bg-white shadow-md rounded-lg px-5 py-10 mt-5">
         <p className="font-bold text-gray-700 uppercase mb-3"> 
-            Title: {' '} 
-            <strong className="font-normal normal-case"> Crear API </strong>
+        Title: {' '} 
+            <strong className="font-normal normal-case"> {title} </strong>
         </p>
 
         <p className="font-bold text-gray-700 uppercase mb-3"> 
             Date: {' '} 
-            <strong className="font-normal normal-case"> 01 Septiembe 2022 </strong>
+            <strong className="font-normal normal-case"> {date} </strong>
         </p>
 
         <p className="font-bold text-gray-700 uppercase mb-3"> 
             Description: {' '} 
-            <strong className="font-normal normal-case"> 
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Consectetur, consequatur, nulla alias vitae animi ea quos commodi totam et numquam autem dolor assumenda 
-                recusandae nam qui laudantium ullam corporis earum? 
-            </strong>
+            <strong className="font-normal normal-case"> {description} </strong>
         </p>
+
+        <div className="flex justify-between">
+          <button className="bg-green-600 hover:bg-green-700 mt-4 py-2 px-10 rounded-md text-white font-bold"
+          type="button" onClick={() => setActualTask(task)}> Update </button>
+
+          <button className="bg-red-600 hover:bg-red-700 mt-4 py-2 px-10 rounded-md text-white font-bold"
+          type="button"> Delete </button>
+        </div>
     </div>
   );
 }
